5fca87f3fa4168f6daea4ad347d9f66dc0cabdcc
Your Name <you@example.com>
2024-05-26 21:31:25 +0530

    displayname_updated

--- display_buildout/src/App.js
+++ display_buildout/src/App.js
@@ -27,7 +27,6 @@ function App() {
             onChange={(e) => setFirstname(e.target.value)}
             required
           />
-          <br></br>
           <label htmlFor="last name">Lastname:</label>
           <input
             type="text"
@@ -35,7 +34,6 @@ function App() {
             onChange={(e) => setlastname(e.target.value)}
             required
           />
-          <br></br>
           <button type="submit">Submit</button>
         </form>
         {fullname && <div>Full name:{fullname}</div>}
@@ -45,3 +43,51 @@ function App() {
 }
 
 export default App;
+
+// import { useState } from 'react';
+// import './App.css';
+
+// function App() {
+
+//   const initialFormData = {
+//     first: "",
+//     last: ""
+//   }
+
+//   const [formData, setFormData] = useState(initialFormData);
+//   const [displayName, setDisplayName] = useState(false);
+
+//   const handleChange = (event) => {
+//     const { name, value } = event.target;
+//     setFormData({
+//       ...formData,
+//       [name]: value,
+//     })
+//   }
+
+//   const handleSubmit = (event) => {
+//     event.preventDefault(); 
+//     setDisplayName(true);
+//     console.log("Form submitted:", formData);
+//   }
+
+//   const { first, last } = formData;
+
+//   return (
+//     <>
+//       <h1>Full Name Display</h1>
+//       <form onSubmit={handleSubmit}>
+//         <label htmlFor="first">First Name
+//           <input type="text" id="first" name="first" required onChange={handleChange} value={first} />
+//         </label>
+//         <label htmlFor="last">Last Name
+//           <input type="text" id="last" name="last" required onChange={handleChange} value={last} />
+//         </label>
+//         <button type="submit">Submit</button>
+//       </form>
+//       {displayName && <p>{`Full Name: ${first} ${last}`}</p>}
+//     </>
+//   )
+// }
+
+// export default App;
