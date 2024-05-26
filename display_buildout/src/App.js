53dcaed11a051019680f54d0db248e85f8f1e036
Your Name <you@example.com>
2024-05-26 21:23:50 +0530

    displayname_updated

--- display_buildout/src/App.js
+++ display_buildout/src/App.js
@@ -1,36 +1,46 @@
-import './App.css';
-import { useState } from 'react';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const [firstname , setFirstname] = useState();
-  const [lastname , setlastname] = useState();
-  const [fullname , setFullname] = useState();
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setlastname] = useState("");
+  const [fullname, setFullname] = useState("");
 
-
-  const handlesubmit = (e) =>{
+  const handlesubmit = (e) => {
     e.preventDefault();
-    if(!firstname || !lastname)
-      {
-        setFullname();
-      }
-    let result = firstname+" "+lastname;
+    if (!firstname || !lastname) {
+      setFullname("");
+    }
+    let result = firstname + " " + lastname;
     setFullname(result);
-  }
+  };
 
   return (
-    <div className="App">
-      <h1>Full name Display</h1>
-      <form onSubmit={handlesubmit}>
-        <label>Firstname:</label>
-        <input type='text' value={firstname} onChange={(e)=>setFirstname(e.target.value)} required/>
-        <br></br>
-        <label>Lastname:</label>
-        <input type='text' value={lastname} onChange={(e)=>setlastname(e.target.value)} required/>
-        <br></br>
-        <button type='submit'>Submit</button>
-      </form>
-      {fullname && <div>Full name:{fullname}</div>}
-    </div>
+    <>
+      <div className="App">
+        <h1>Full name Display</h1>
+        <form onSubmit={handlesubmit}>
+          <label htmlFor="first name">Firstname:</label>
+          <input
+            type="text"
+            value={firstname}
+            onChange={(e) => setFirstname(e.target.value)}
+            required
+          />
+          <br></br>
+          <label htmlFor="last name">Lastname:</label>
+          <input
+            type="text"
+            value={lastname}
+            onChange={(e) => setlastname(e.target.value)}
+            required
+          />
+          <br></br>
+          <button type="submit">Submit</button>
+        </form>
+        {fullname && <div>Full name:{fullname}</div>}
+      </div>
+    </>
   );
 }
 
